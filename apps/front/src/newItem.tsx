22c9e886385f9be66df41462fc935d3f7c73ddce
Samoth69 <thomasviolent@gmail.com>
2024-01-31 10:17:36 +0100

    react hook form pour newItem

--- apps/front/src/newItem.tsx
+++ apps/front/src/newItem.tsx
@@ -2,12 +2,11 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { UserTask } from "shared/src/model";
 
 export function NewItem() {
   const queryClient = useQueryClient();
-  const [textInput, setTextInput] = useState("");
   const createTodo = useMutation({
     mutationFn: (newTodo: UserTask) =>
       axios
@@ -18,30 +17,24 @@ export function NewItem() {
       })
     }
   })
+  const { register, handleSubmit, reset } = useForm<UserTask>()
+  const onSubmit: SubmitHandler<UserTask> = (data) => {
+    createTodo.mutate(data);
+    reset();
+  };
 
-  const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === "Enter") {
-      addNewItem();
-    }
-  }
-
-  const addNewItem = () => {
-    createTodo.mutate({ id: undefined, checked: undefined, text: textInput });
-    setTextInput("");
-  }
   return (
-    <div className="flex flex-row items-stretch gap-2 p-0.5 border rounded-md">
+    <form className="flex flex-row items-stretch gap-2 p-0.5 border rounded-md" onSubmit={handleSubmit(onSubmit)}>
       <input
         className="grow"
-        value={textInput}
-        onChange={e => setTextInput(e.target.value)}
-        onKeyDown={(e) => onKeyDown(e)} />
-      <div
+        {...register("text", { required: true })}
+      />
+      <button
         className="shrink-0 size-[26px] flex items-center rounded-md hover:bg-blue-100 hover:text-blue-500"
-        onClick={() => addNewItem()}
+        type="submit"
       >
         <FontAwesomeIcon icon={faPlus} className="flex-1" />
-      </div>
-    </div>
+      </button>
+    </form>
   )
 }
