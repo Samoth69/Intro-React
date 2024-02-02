1e86d9831f569f159924e069f6449e822cf9a215
Samoth69 <thomasviolent@gmail.com>
2024-02-02 14:24:31 +0100

    prisma undefined, on évite de seed si la db à déjà des données

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,29 @@
 import prisma from '../lib/prisma'
 
 async function main() {
-  const response = await Promise.all([
-    await prisma.task.create({
-      data: {
-        text: "Construire une appli next js",
-        checked: 0
-      }
-    }),
-    await prisma.task.create({
-      data: {
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        checked: 1
-      }
-    }),
-    await prisma.task.create({
-      data: {
-        text: "TEST TEST TEST",
-        checked: 1
-      }
-    })
-  ])
-  console.log(response)
+  if (await prisma.task.count() === 0) {
+    const response = await Promise.all([
+      await prisma.task.create({
+        data: {
+          text: "Construire une appli next js",
+          checked: 0
+        }
+      }),
+      await prisma.task.create({
+        data: {
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          checked: 1
+        }
+      }),
+      await prisma.task.create({
+        data: {
+          text: "TEST TEST TEST",
+          checked: 1
+        }
+      })
+    ])
+    console.log(response)
+  }
 }
 main()
   .then(async () => {
